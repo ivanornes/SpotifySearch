3faaa7c00b16cbf2082460ccd40cd08b96d31200
Ivan <ivan.ornes@gmail.com>
2021-01-22 22:12:23 +0100

    Created SpotifyAPI class for searching the music data

---
+++ SpotifyAPI/SpotifyAPI/SpotifyAPI.h
@@ -0,0 +1,18 @@
+//
+//  SpotifyAPI.h
+//  SpotifyAPI
+//
+//  Created by Ivan Ornes on 22/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotifyAPI.
+FOUNDATION_EXPORT double SpotifyAPIVersionNumber;
+
+//! Project version string for SpotifyAPI.
+FOUNDATION_EXPORT const unsigned char SpotifyAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotifyAPI/PublicHeader.h>
+
+
---
+++ SpotifyAPI/SpotifyAPITests/SpotifyAPITests.swift
@@ -0,0 +1,41 @@
+//
+//  SpotifyAPITests.swift
+//  SpotifyAPITests
+//
+//  Created by Ivan Ornes on 22/1/21.
+//
+
+import XCTest
+@testable import SpotifyAPI
+
+struct SearchItem {
+}
+
+class SpotifyAPI {
+    typealias SearchResult = Result<[SearchItem], Swift.Error>
+    
+    func search(_ text: String, onCompletion: @escaping (SearchResult) -> Void) {
+        onCompletion(.success([]))
+    }
+}
+
+class SearchSpotifyAPITests: XCTestCase {
+
+    func test_searchRequest_returnsEmptyList() {
+        let sut = makeSUT()
+        
+        sut.search("random-music-string-search") { result in
+            switch result {
+            case let .success(searchResults): XCTAssert(searchResults.isEmpty)
+            case let .failure(error): XCTFail("Expected to fetch some data, got \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // MARK: Helper
+    
+    func makeSUT() -> SpotifyAPI {
+        let sut = SpotifyAPI()
+        return sut
+    }
+}
