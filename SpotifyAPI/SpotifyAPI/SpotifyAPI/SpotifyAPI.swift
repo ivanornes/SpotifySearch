c59810e65b89a6284c0c15625d24cd640a44f6cb
Ivan <ivan.ornes@gmail.com>
2021-01-23 17:19:11 +0100

    Added offset and limit to the `SearchCriteria`

--- SpotifyAPI/SpotifyAPI/SpotifyAPI/SpotifyAPI.swift
+++ SpotifyAPI/SpotifyAPI/SpotifyAPI/SpotifyAPI.swift
@@ -14,6 +14,15 @@ public enum SearchType: String {
 public struct SearchCriteria {
     public let text: String
     public let type: SearchType
+    public let offset: UInt
+    public let limit: UInt
+    
+    public init(text: String, type: SearchType, offset: UInt = 0, limit: UInt = 20) {
+        self.text = text
+        self.type = type
+        self.offset = offset
+        self.limit = limit
+    }
 }
 
 public class SpotifyAPI {
