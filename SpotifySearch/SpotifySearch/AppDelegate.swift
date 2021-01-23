c94955bcf400a3ea6fd401993a2c8037683c7f0e
Ivan <ivan.ornes@gmail.com>
2021-01-23 08:13:25 +0100

    Created the `SpotifySeach` iOS app project

---
+++ SpotifySearch/SpotifySearch/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  SpotifySearch
+//
+//  Created by Ivan Ornes on 23/1/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ SpotifySearch/SpotifySearch/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  SpotifySearch
+//
+//  Created by Ivan Ornes on 23/1/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    private lazy var navigationController = UINavigationController()
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        configureWindow()
+    }
+    
+    func configureWindow() {
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
